f5de73a595886bac0ecf7494168cebc0dcaaffd8
Valerii <mandryka.valerii@gmail.com>
2022-10-21 00:37:51 +0300

    modified:   cypress.config.js 	new file:   cypress/e2e/features/saveTelnyxForm.feature 	new file:   cypress/e2e/pageObjects/LoginPage.js 	new file:   cypress/e2e/pageObjects/commonPage.js 	new file:   cypress/e2e/pageObjects/createAccountPage.js 	new file:   cypress/e2e/pageObjects/mainPage.js 	new file:   cypress/e2e/step_definitions/saveTelnyxForm.js 	deleted:    cypress/e2e/tests/Specs/todo.cy.js 	modified:   package-lock.json 	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,26 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
     video:false,
     experimentalStudio: true,
-    baseUrl: 'https://example.cypress.io/todo',
+    chromeWebSecurity: false,
+    //baseUrl: 'https://example.cypress.io/todo',
     reporter: 'mocha-allure-reporter',
     excludeSpecPattern: 'cypress/e2e/2-advanced-examples/*.js',
-    specPattern: 'cypress/e2e/**/*.js',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/**/*.{feature,features}',
+    setupNodeEvents
   },
 });
